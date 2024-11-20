00071fdfef50e287a0a29484189598b7336c9c43
kuwapica <annidaaiska@gmail.com>
2024-11-21 02:00:28 +0700

    refactor styles, enhance banner functionality, and implement form validation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,50 @@
-// ini javascript
+// untuk validasi form
+function formValidate() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const interest = document.getElementById('interest').value;
+
+    if (name === '') {
+        alert('please enter your name');
+        return false;
+    } else if (email === '') {
+        alert('please enter your email');
+        return false;   
+    } else if (interest === '') {
+        alert('please select your interest');
+        return false;
+    } else {
+        alert('Form submitted successfully');
+        return true;
+    }
+}
+
+
+// untuk banner auto slide
+let indexImage = 1;
+const images = document.getElementsByClassName('banner-autoslide');
+
+nextImage();
+
+function nextImage() {
+    showImage(indexImage += 1);
+}
+
+function showImage(index) {
+    // reset biar kalo udah mentok bakal balik ke awal
+    if (index > images.length) indexImage = 1;
+    hideAllImages();
+    images[indexImage - 1].style.display = 'block'; // index -1 karena index array dimulai dari 0
+}
+
+
+function hideAllImages() {
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = 'none';
+    }
+}
+
+setInterval(() => nextImage(), 2500);
+
+
+
